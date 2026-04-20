235a470302a30d1aed3c317dc5f28b553472e69d
Nusss0 <stevanus610@gmail.com>
2026-04-20 20:33:14 +0700

    feat : printBoard and printProperty

--- src/views/formatter.hpp
+++ src/views/formatter.hpp
@@ -6,13 +6,18 @@ using namespace std;
 
 class OutputFormatter {
     private:
+        const string RESET   = "\033[0m";
+        const string RED     = "\033[31m";
+        const string GREEN   = "\033[32m";
+        const string YELLOW  = "\033[33m";
+        const string BLUE    = "\033[34m";
+        const string MAGENTA = "\033[35m";
+        const string WHITE   = "\033[37m";
         string getGroupColor(string colorGroup);
-        //To Print a centered text with padding
         string centerOut(string str, int width);
-        //To Print a left text with padding
         string leftOut(string str, int width);
-        //To print a colored text
-        string colorText(string str,string color);
+        string colorText(string str, string color);
+        vector<string> renderTile(string code, string name, string colorGroup, int w, int h);
     public:
         void printBoard(Board &b);
         void printAkta(PropertyTile &t);
